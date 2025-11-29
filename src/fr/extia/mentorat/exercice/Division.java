7b12a900e56c06d41c1dc4c7e9cae45fbe6db6ec
antoine-lepage <antoine.lepage98@gmail.com>
2025-11-29 15:39:15 +0100

    Add comments for exercise complexity levels

--- src/fr/extia/mentorat/exercice/Division.java
+++ src/fr/extia/mentorat/exercice/Division.java
@@ -7,6 +7,11 @@ public class Division {
      * 
      * Entrée : deux entiers a et b
      * Sortie : le quotient et le reste de la division euclidienne de a par b
+     *
+     * Plusieurs niveaux de complexité pour cet exercice :
+     *    - 1 - Définir a et b en dur dans la méthode main
+     *    - 2 - Vous demandez à l'utilisateur de renseignez le dividende et diviseur (Facultatif)
+     *        - Ressource : https://www.w3schools.com/java/java_user_input.asp
      */
     public static void main(String[] args) {
         //point d'entrée du programme

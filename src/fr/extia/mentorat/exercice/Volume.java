038d1f3b249d84133ed194ce8f3a83bbaea8b271
antoine-lepage <antoine.lepage98@gmail.com>
2025-11-29 15:36:15 +0100

    Update Volume.java

--- src/fr/extia/mentorat/exercice/Volume.java
+++ src/fr/extia/mentorat/exercice/Volume.java
@@ -7,6 +7,15 @@ public class Volume {
      * 
      * Entrée : rayon d'une sphère
      * Sortie : volume de la sphère (V = 4/3 * π * r³)
+     *
+     * Plusieurs niveaux de réalisations pour cet exercice :
+     *    - 1 - Définir π = 3.14 et afficher le résultat dans la console
+     *    - 2 - Utiliser PI fournit par Java
+     *        - Ressource : https://docs.oracle.com/javase/8/docs/api/java/lang/Math.html#PI
+     *    - 3 - Tronquer le résultat à 2 chiffres après la virgule
+     *        - Ressources :
+     *                - Math.floor : https://www.w3schools.com/java/ref_math_floor.asp
+     *                - DecimalFormat : https://docs.oracle.com/javase/8/docs/api/java/text/DecimalFormat.html / https://www.baeldung.com/java-decimalformat
      */
     public static void main(String[] args) {
         //Point d'entrée du programme

cc4ba80e78c4470151e54aab378215e266a32016
antoine-lepage <antoine.lepage98@gmail.com>
2025-11-29 15:28:15 +0100

    Enhance comments for exercise instructions in Resume.java
    Added comments explaining the exercise and user input levels.

--- src/fr/extia/mentorat/exercice/Resume.java
+++ src/fr/extia/mentorat/exercice/Resume.java
@@ -7,6 +7,11 @@ public class Resume {
      * 
      * Entrées : Nom, Prénom, Civilité, Année de naissance, téléphone, etc.
      * Sortie : Un résumé formaté (M. Dupont Jean, né en 1980, téléphone : 0123456789)
+     *
+     * 2 niveaux de réalisations pour cet exercice :
+     *    - 1er niveau : Vous renseignez les variables et leur valeur en dur dans la méthode main
+     *    - 2ème niveau : Vous demandez à l'utilisateur de renseignez les informations du résumé (Facultatif)
+     *        - Ressource : https://www.w3schools.com/java/java_user_input.asp
      */
     public static void main(String[] args) {
         //Point d'entrée du programme
